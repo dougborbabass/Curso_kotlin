c732741d4def5035ebb5b71458606608ac8eec38
Douglas Borba <douglas.borba@acad.pucrs.br>
2020-03-20 10:30:18 -0300

    laço for

---
+++ intro_linguagem/src/flowFor.kt
@@ -0,0 +1,16 @@
+fun main (){
+
+    val str = "Douglas Borba"
+
+    for(c in str){
+        println("$c")
+    }
+
+    for(i in 0..100 step 2){
+        println("$i")
+    }
+
+    for(j in 100 downTo  0){
+        println("$j")
+    }
+}
