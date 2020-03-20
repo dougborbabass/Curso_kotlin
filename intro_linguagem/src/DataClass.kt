dcd77c031ff1b60c65af44cc494c04c467ceae16
Douglas Borba <douglas.borba@acad.pucrs.br>
2020-03-20 12:35:29 -0300

    data class

---
+++ intro_linguagem/src/DataClass.kt
@@ -0,0 +1,33 @@
+import com.sun.org.apache.xpath.internal.operations.Bool
+
+fun main(){
+    val q1 = Quadrado(10f)
+    val q2 = Quadrado(10f)
+    val t1 = Triangulo(10f)
+    val t2 = Triangulo(10f)
+
+    //aqui teriamos que sobrescrever o toString para termos o valor
+    println(q1)
+    //o data class já imprime o valor do atributo
+    println(t1)
+
+    //aqui estaria comparando as posições de uma memória
+    println(q1 == q2)
+    //aqui já compara os valores dos atribuitos pois é uma data class
+    println(t1 == t2)
+
+}
+
+class Quadrado(val area: Float)
+
+data class Triangulo(val area: Float)
+
+// a data class serve para transitar dados como parametros
+data class Param(var b: Boolean, var s: String, var i: Int)
+fun teste(p: Param){
+
+}
+
+//fun teste(b: Boolean, s: String, i: Int){
+//
+//}
