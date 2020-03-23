003d8048b6e4773079cec94a515c1e8a5b330b42
Douglas Borba <douglas.borba@acad.pucrs.br>
2020-03-23 12:04:26 -0300

    contact business

--- agenda/src/business/ContactBusiness.kt
+++ agenda/src/business/ContactBusiness.kt
@@ -1,23 +1,35 @@
 package business
 
+import entity.ContactEntity
+import repository.ContactRepository
 import java.lang.Exception
 
 class ContactBusiness {
     fun save(name: String, phone: String) {
         validate(name, phone)
+        val contact = ContactEntity(name, phone)
+
+        ContactRepository.save(contact)
     }
 
     fun delete(name: String, phone: String) {
         validateDelete(name, phone)
+        val contact = ContactEntity(name, phone);
+
+        ContactRepository.delete(contact)
     }
 
-    fun validateDelete(name: String, phone: String) {
+    fun getList(): MutableList<ContactEntity> {
+        return ContactRepository.getList()
+    }
+
+    private fun validateDelete(name: String, phone: String) {
         if (name == "" || phone == "") {
             throw Exception("É necessário selecionar um contato antes de remover")
         }
     }
 
-    fun validate(name: String, phone: String) {
+    private fun validate(name: String, phone: String) {
         if (name == "") {
             throw Exception("Nome é obrigatório")
         }
--- agenda/src/ui/MainForm.java
+++ agenda/src/ui/MainForm.java
@@ -1,15 +1,21 @@
 package ui;
 
+import business.ContactBusiness;
+import entity.ContactEntity;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.List;
 
 public class MainForm extends JFrame {
     private JPanel rootPanel;
     private JButton buttonNewContact;
     private JButton buttonRemove;
     private JTable tableContacts;
+
+    private ContactBusiness mContactBusiness;
 
     public MainForm(){
         setContentPane(rootPanel);
@@ -21,8 +27,10 @@ public class MainForm extends JFrame {
 
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
+        mContactBusiness = new ContactBusiness();
 
         setListeners();
+        loadContacts();
     }
 
     private void setListeners() {
@@ -40,5 +48,9 @@ public class MainForm extends JFrame {
 
             }
         });
+    }
+
+    private void loadContacts() {
+       List<ContactEntity> contactList = mContactBusiness.getList();
     }
 }
