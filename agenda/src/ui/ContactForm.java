7b00fdbb9e8eae43411277f58bb9de7dad39fa37
Douglas Borba <douglas.borba@acad.pucrs.br>
2020-03-23 13:53:04 -0300

    salvando e listando

--- agenda/src/ui/ContactForm.java
+++ agenda/src/ui/ContactForm.java
@@ -1,5 +1,7 @@
 package ui;
 
+import business.ContactBusiness;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -13,7 +15,9 @@ public class ContactForm extends JFrame {
     private JButton buttonCancel;
     private JButton buttonSave;
 
-    public ContactForm(){
+    private ContactBusiness mContactBusiness;
+
+    public ContactForm() {
         setContentPane(rootPanel);
         setSize(500, 250);
         setVisible(true);
@@ -22,7 +26,9 @@ public class ContactForm extends JFrame {
         setLocation(dim.width / 2 - getSize().width / 2, dim.height / 2 - getSize().height / 2);
 
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        
+
+        mContactBusiness = new ContactBusiness();
+
         setListeners();
 
     }
@@ -32,6 +38,18 @@ public class ContactForm extends JFrame {
             @Override
             public void actionPerformed(ActionEvent e) {
 
+                try {
+                    String name = textName.getText();
+                    String phone = textPhone.getText();
+
+                    mContactBusiness.save(name, phone);
+
+                    new MainForm();
+                    dispose();
+                    
+                } catch (Exception excp) {
+                    JOptionPane.showMessageDialog(new JFrame(), excp.getMessage());
+                }
             }
         });
 
