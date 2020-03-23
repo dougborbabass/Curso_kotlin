c8f2cf81f351ee152364b531ede6a516313d7cf9
Douglas Borba <douglas.borba@acad.pucrs.br>
2020-03-23 16:05:53 -0300

    onclick

--- gasto_de_viagem/app/src/main/java/com/douglasborba/gastodeviagem/MainActivity.kt
+++ gasto_de_viagem/app/src/main/java/com/douglasborba/gastodeviagem/MainActivity.kt
@@ -2,11 +2,28 @@ package com.douglasborba.gastodeviagem
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        buttonCalculate.setOnClickListener(this)
+
+    }
+
+    override fun onClick(view: View) {
+        val id = view.id
+
+        if(id == R.id.buttonCalculate){
+            calculate()
+        }
+    }
+
+    private fun calculate() {
+
     }
 }
