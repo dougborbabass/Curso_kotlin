5d850578162cd9ce764b67c39c8e92d2ccc0781b
Douglas Borba <douglas.borba@acad.pucrs.br>
2020-03-23 14:30:59 -0300

    removendo contato

--- agenda/src/ui/MainForm.java
+++ agenda/src/ui/MainForm.java
@@ -4,6 +4,8 @@ import business.ContactBusiness;
 import entity.ContactEntity;
 
 import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -16,6 +18,7 @@ public class MainForm extends JFrame {
     private JButton buttonRemove;
     private JTable tableContacts;
     private JLabel labelContactCount;
+    private String mName, mPhone = "";
 
     private ContactBusiness mContactBusiness;
 
@@ -44,10 +47,32 @@ public class MainForm extends JFrame {
             }
         });
 
+        tableContacts.getSelectionModel().addListSelectionListener(new ListSelectionListener() {
+            @Override
+            public void valueChanged(ListSelectionEvent e) {
+
+                if (e.getValueIsAdjusting()) {
+                    if(tableContacts.getSelectedRow() != -1){
+                        mName = tableContacts.getValueAt(tableContacts.getSelectedRow(), 0).toString();
+                        mPhone = tableContacts.getValueAt(tableContacts.getSelectedRow(), 1).toString();
+                    }
+                }
+            }
+        });
+
         buttonRemove.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                try {
+                    mContactBusiness.delete(mName, mPhone);
+                    loadContacts();
 
+                    mName = "";
+                    mPhone = "";
+
+                } catch (Exception excp) {
+                    JOptionPane.showMessageDialog(new JFrame(), excp.getMessage());
+                }
             }
         });
     }
