fee8a9ea81d41769c3eff0118fb3e1b384bb079e
Douglas Borba <douglas.borba@acad.pucrs.br>
2020-03-23 11:12:47 -0300

    actions listeners

--- agenda/src/ui/ContactForm.java
+++ agenda/src/ui/ContactForm.java
@@ -2,6 +2,8 @@ package ui;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class ContactForm extends JFrame {
 
@@ -16,5 +18,29 @@ public class ContactForm extends JFrame {
         setSize(500, 250);
         setVisible(true);
 
+        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+        setLocation(dim.width / 2 - getSize().width / 2, dim.height / 2 - getSize().height / 2);
+
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        
+        setListeners();
+
+    }
+
+    private void setListeners() {
+        buttonSave.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        buttonCancel.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new MainForm();
+                dispose();
+            }
+        });
     }
 }
