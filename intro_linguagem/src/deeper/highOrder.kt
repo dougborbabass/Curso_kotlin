6a08e0e5ac0be7b41f98e3bb49fb16b0e242676c
Douglas Borba <douglas.borba@acad.pucrs.br>
2020-03-24 11:54:04 -0300

    função chamando função

---
+++ intro_linguagem/src/deeper/highOrder.kt
@@ -0,0 +1,23 @@
+package deeper
+
+fun main(){
+    operator(1,2, :: mult)
+    operator(1,2, :: sum)
+
+    val list = listOf(1,2,3)
+    list.paraCada { println(it) }
+
+    val str = listOf("oi","oi2", "oi3", "oi4")
+    str.paraCada { println(it) }
+}
+
+fun operator(x: Int, y: Int, op: (Int, Int) -> Int) : Int {
+    return op(x, y)
+}
+
+fun <T> Iterable<T>.paraCada(op: (T) -> Unit){
+    for (i in this) op(i)
+}
+
+fun sum(x: Int, y: Int) = x + y
+fun mult(x: Int, y: Int) = x * y
