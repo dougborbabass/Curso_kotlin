b8f11dee9cf344b45ad601132401226e59cfce1b
Douglas Borba <douglas.borba@acad.pucrs.br>
2020-03-31 16:22:10 -0300

    update

--- Convidados/app/src/main/java/com/douglasborba/convidados/service/repository/GuestRepository.kt
+++ Convidados/app/src/main/java/com/douglasborba/convidados/service/repository/GuestRepository.kt
@@ -26,15 +26,14 @@ class GuestRepository private constructor(context: Context) {
     }
 
     fun save(guest: GuestModel): Boolean {
-
         return try {
             val db = mGuestDataBaseHelper.writableDatabase
 
-            val content = ContentValues()
-            content.put(DataBaseConstants.GUEST.COLUMNS.NAME, guest.name)
-            content.put(DataBaseConstants.GUEST.COLUMNS.PRESENCE, guest.presence)
+            val contentValues = ContentValues()
+            contentValues.put(DataBaseConstants.GUEST.COLUMNS.NAME, guest.name)
+            contentValues.put(DataBaseConstants.GUEST.COLUMNS.PRESENCE, guest.presence)
 
-            db.insert(DataBaseConstants.GUEST.TABLE_NAME, null, content)
+            db.insert(DataBaseConstants.GUEST.TABLE_NAME, null, contentValues)
 
             true
 
@@ -43,7 +42,24 @@ class GuestRepository private constructor(context: Context) {
         }
     }
 
-    fun update(guest: GuestModel) {
+    fun update(guest: GuestModel): Boolean {
+        return try {
+            val db = mGuestDataBaseHelper.writableDatabase
+
+            val contentValues = ContentValues()
+            contentValues.put(DataBaseConstants.GUEST.COLUMNS.NAME, guest.name)
+            contentValues.put(DataBaseConstants.GUEST.COLUMNS.PRESENCE, guest.presence)
+
+            val selection = DataBaseConstants.GUEST.COLUMNS.ID + " = ?"
+            val args = arrayOf(guest.id.toString())
+
+            db.update(DataBaseConstants.GUEST.TABLE_NAME, contentValues, selection, args)
+
+            true
+
+        } catch (e: Exception) {
+            false
+        }
     }
 
     fun delete(guest: GuestModel) {
