0cf6e9b65cbc7e9d02b9b38029266e8b2fdb107d
Douglas Borba <douglas.borba@acad.pucrs.br>
2020-03-31 16:07:19 -0300

    save on database

--- Convidados/app/src/main/java/com/douglasborba/convidados/service/repository/GuestRepository.kt
+++ Convidados/app/src/main/java/com/douglasborba/convidados/service/repository/GuestRepository.kt
@@ -1,9 +1,12 @@
 package com.douglasborba.convidados.service.repository
 
+import android.content.ContentValues
 import android.content.Context
+import com.douglasborba.convidados.service.constants.DataBaseConstants
 import com.douglasborba.convidados.service.model.GuestModel
+import java.lang.Exception
 
-class GuestRepository private constructor(context: Context){
+class GuestRepository private constructor(context: Context) {
 
     private var mGuestDataBaseHelper: GuestDataBaseHelper = GuestDataBaseHelper(context)
 
@@ -13,38 +16,52 @@ class GuestRepository private constructor(context: Context){
         private lateinit var repository: GuestRepository
 
         // somente o metodo pode dar uma instancia da classe
-        fun getInstance(context: Context): GuestRepository{
+        fun getInstance(context: Context): GuestRepository {
             //se ainda nao foi inicializada, inicialize
-            if(!::repository.isInitialized){
+            if (!::repository.isInitialized) {
                 repository = GuestRepository(context)
             }
-           return repository
+            return repository
         }
     }
 
-    fun save(guest: GuestModel){
-        
+    fun save(guest: GuestModel): Boolean {
+
+        return try {
+            val db = mGuestDataBaseHelper.writableDatabase
+
+            val content = ContentValues()
+            content.put(DataBaseConstants.GUEST.COLUMNS.NAME, guest.name)
+            content.put(DataBaseConstants.GUEST.COLUMNS.PRESENCE, guest.presence)
+
+            db.insert(DataBaseConstants.GUEST.TABLE_NAME, null, content)
+
+            true
+
+        } catch (e: Exception) {
+            false
+        }
     }
 
-    fun update(guest: GuestModel){
+    fun update(guest: GuestModel) {
     }
 
-    fun delete(guest: GuestModel){
+    fun delete(guest: GuestModel) {
     }
 
-    fun getAll(): List<GuestModel>{
+    fun getAll(): List<GuestModel> {
         val list: MutableList<GuestModel> = ArrayList()
 
         return list
     }
 
-    fun getPresent(): List<GuestModel>{
+    fun getPresent(): List<GuestModel> {
         val list: MutableList<GuestModel> = ArrayList()
 
         return list
     }
 
-    fun getAbsent(): List<GuestModel>{
+    fun getAbsent(): List<GuestModel> {
         val list: MutableList<GuestModel> = ArrayList()
 
         return list
