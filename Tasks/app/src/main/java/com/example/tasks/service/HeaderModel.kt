cf26367e85b2dccc57ad7e17b6475270fc6a3d8e
Douglas Borba <douglas.borba@acad.pucrs.br>
2020-04-20 15:14:54 -0300

    login finalizado

--- Tasks/app/src/main/java/com/example/tasks/service/HeaderModel.kt
+++ Tasks/app/src/main/java/com/example/tasks/service/HeaderModel.kt
@@ -5,7 +5,7 @@ import com.google.gson.annotations.SerializedName
 class HeaderModel {
 
     @SerializedName("token")
-    var toker: String = ""
+    var token: String = ""
     @SerializedName("personKey")
     var personKey: String = ""
     @SerializedName("name")
--- Tasks/app/src/main/java/com/example/tasks/service/repository/PersonRepository.kt
+++ Tasks/app/src/main/java/com/example/tasks/service/repository/PersonRepository.kt
@@ -1,27 +1,41 @@
 package com.example.tasks.service.repository
 
+import android.content.Context
+import com.example.tasks.R
 import com.example.tasks.service.HeaderModel
+import com.example.tasks.service.constants.TaskConstants
+import com.example.tasks.service.listener.APIListener
 import com.example.tasks.service.repository.remote.PersonService
 import com.example.tasks.service.repository.remote.RetrofitClient
+import com.google.gson.Gson
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class PersonRepository {
+class PersonRepository(val context: Context) {
 
     private val mRemote = RetrofitClient.createService(PersonService::class.java)
 
-    fun login(email: String, password: String){
+    fun login(email: String, password: String, listener: APIListener){
 
         val call: Call<HeaderModel> = mRemote.login(email, password)
         // Assíncrona
         call.enqueue(object : Callback<HeaderModel>{
             override fun onResponse(call: Call<HeaderModel>, response: Response<HeaderModel>) {
-                val header = response.body()
+
+                if (response.code() != TaskConstants.HTTP.SUCCESS){
+                    // o errorbody ainda esta em json, por isso deve ser feito a conversão da msg antes de enviar para a interface
+                    val validation = Gson().fromJson(response.errorBody().toString(), String::class.java)
+                    listener.onFailure(validation)
+                } else {
+                    response.body()?.let {
+                        listener.onSuccess(it)
+                    }
+                }
             }
 
             override fun onFailure(call: Call<HeaderModel>, t: Throwable) {
-                val s = ""
+                listener.onFailure(context.getString(R.string.ERROR_UNEXPECTED))
             }
 
         })
