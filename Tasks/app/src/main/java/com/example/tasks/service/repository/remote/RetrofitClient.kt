bfe1886a56c619266a2f4235f3dd9361c3ee308a
Douglas Borba <douglas.borba@acad.pucrs.br>
2020-04-28 11:36:41 -0300

    implementação do header na chamada de retrofit

--- Tasks/app/src/main/java/com/example/tasks/service/repository/remote/RetrofitClient.kt
+++ Tasks/app/src/main/java/com/example/tasks/service/repository/remote/RetrofitClient.kt
@@ -1,6 +1,9 @@
 package com.example.tasks.service.repository.remote
 
+import com.example.tasks.service.constants.TaskConstants
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
+import okhttp3.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -10,10 +13,25 @@ class RetrofitClient private constructor() {
 
         private lateinit var mRetrofit: Retrofit
         private const val baseUrl = "http://devmasterteam.com/CursoAndroidAPI/"
+        private var personKey = ""
+        private var tokenKey = ""
 
         private fun getRetrofitInstance(): Retrofit {
-
             val httpClient = OkHttpClient.Builder()
+
+//            httpClient.addInterceptor(object : Interceptor{
+//                override fun intercept(chain: Interceptor.Chain): Response {
+//                    val request =
+//                        chain.request()
+//                            .newBuilder()
+//                            .addHeader(TaskConstants.HEADER.PERSON_KEY, personKey)
+//                            .addHeader(TaskConstants.HEADER.TOKEN_KEY, tokenKey)
+//                            .build()
+//
+//                    return chain.proceed(request)
+//                }
+//            })
+
             if (!Companion::mRetrofit.isInitialized) {
                 mRetrofit = Retrofit.Builder()
                     .baseUrl(baseUrl)
@@ -22,6 +40,11 @@ class RetrofitClient private constructor() {
                     .build()
             }
             return mRetrofit
+        }
+
+        fun addHeader(token: String, personKey: String){
+            this.personKey = personKey
+            this.tokenKey = token
         }
 
         fun <T> createService(serviceClass: Class<T>): T {
